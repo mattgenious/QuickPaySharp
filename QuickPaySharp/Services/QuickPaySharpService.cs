be45cb7ce65237098068d0f1f095ed14ce9ce25c
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2020-10-27 17:47:17 +0100

    Added some boilerplate from RechargeSharp to manage http requests

---
+++ QuickPaySharp/Services/QuickPaySharpService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Retry;
+
+namespace QuickPaySharp.Services
+{
+    public class QuickPaySharpService
+    {
+        protected readonly AsyncRetryPolicy<HttpResponseMessage> AsyncRetryPolicy;
+
+        private readonly ILogger<QuickPaySharpService> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly QuickPaySharpServiceOptions _rechargeServiceOptions;
+        private readonly HttpClient _client;
+        protected QuickPaySharpService(ILogger<QuickPaySharpService> logger, IHttpClientFactory httpClientFactory, IOptions<QuickPaySharpServiceOptions> quickPaySharpServiceOptions)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _rechargeServiceOptions = quickPaySharpServiceOptions.Value;
+
+            var apiKey = _rechargeServiceOptions.GetApiKey();
+
+            _client = _httpClientFactory.CreateClient("RechargeSharpClient");
+            _client.DefaultRequestHeaders.Remove("X-Recharge-Access-Token");
+            _client.DefaultRequestHeaders.Add("X-Recharge-Access-Token", apiKey);
+
+            AsyncRetryPolicy = Policy.HandleResult<HttpResponseMessage>(x =>
+            {
+                if (!x.IsSuccessStatusCode)
+                {
+                    _logger.LogError("X-Request-Id:" + string.Join(",", x.Headers.GetValues("X-Request-Id")));
+                    _logger.LogError(x.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                    if ((int)x.StatusCode == 401)
+                    {
+                        throw new UnauthorizedAccessException($"Api key starts with: {apiKey.Take(4)} was unauthorized");
+                    }
+                    if (x.StatusCode == HttpStatusCode.TooManyRequests)
+                    {
+                        var newApiKey = _rechargeServiceOptions.GetApiKey();
+                        _client.DefaultRequestHeaders.Remove("X-Recharge-Access-Token");
+                        _client.DefaultRequestHeaders.Add("X-Recharge-Access-Token", newApiKey);
+                        _logger.LogInformation($"changed apikey to: {newApiKey}");
+                        return true;
+                    }
+                    if ((int)x.StatusCode > 399 && x.StatusCode != HttpStatusCode.TooManyRequests && (x.StatusCode != HttpStatusCode.NotFound && x.RequestMessage.Method != HttpMethod.Get))
+                    {
+                        throw new Exception(x.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                    }
+
+                    if (x.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        throw new Exception("Returned 404");
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }).WaitAndRetryForeverAsync(
+                retryAttempt =>
+                    TimeSpan.FromSeconds(3));
+        }
+
+        protected Task<HttpResponseMessage> GetAsync(string path)
+        {
+            _logger.LogInformation($"RechargeSharp GET: {path}");
+            return AsyncRetryPolicy.ExecuteAsync(async () => await _client.GetAsync(path));
+        }
+        protected Task<HttpResponseMessage> PutAsJsonAsync(string path, string jsonData)
+        {
+            _logger.LogInformation($"RechargeSharp PUT: {path}\r\nJSONDATA: {jsonData}");
+            var content = new StringContent(jsonData);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return AsyncRetryPolicy.ExecuteAsync(async () => await _client.PutAsync(path, content));
+        }
+        protected Task<HttpResponseMessage> PostAsJsonAsync(string path, string jsonData)
+        {
+            _logger.LogInformation($"RechargeSharp POST: {path}\r\nJSONDATA: {jsonData}");
+            var content = new StringContent(jsonData);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return AsyncRetryPolicy.ExecuteAsync(async () => await _client.PostAsync(path, content));
+        }
+        protected Task<HttpResponseMessage> DeleteAsync(string path)
+        {
+            _logger.LogInformation($"RechargeSharp DELETE: {path}");
+            return AsyncRetryPolicy.ExecuteAsync(async () => await _client.DeleteAsync(path));
+        }
+
+        protected void ValidateModel(object objectToValidate)
+        {
+            var context = new ValidationContext(objectToValidate);
+            var results = new List<ValidationResult>();
+
+            if (!Validator.TryValidateObject(objectToValidate, context, results, true))
+            {
+                throw new ArgumentException(string.Join(", ", results), nameof(objectToValidate));
+            }
+        }
+    }
+}
---
+++ QuickPaySharp/Services/QuickPaySharpServiceCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http.Headers;
+using QuickPaySharp.Services;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class QuickPaySharpServiceCollection
+    {
+        public static IServiceCollection AddRechargeSharp(this IServiceCollection services, Action<QuickPaySharpServiceOptions> options)
+        {
+            services.Configure(options);
+
+            services.AddHttpClient("RechargeSharpClient", (services, opts) =>
+            {
+                opts.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                opts.BaseAddress = new Uri("https://api.rechargeapps.com/");
+            });
+
+            services.AddHttpClient("RechargeSharpWebhookClient", (services, opts) =>
+            {
+                opts.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                opts.BaseAddress = new Uri("https://api.rechargeapps.com/");
+            });
+
+            services.AddTransient(x => options);
+            services.AddLogging();
+
+            //services.AddTransient<AddressService>()
+            //    .AddTransient<ChargeService>()
+            //    .AddTransient<CheckoutService>()
+            //    .AddTransient<CollectionService>()
+            //    .AddTransient<CustomerService>()
+            //    .AddTransient<DiscountService>()
+            //    .AddTransient<MetafieldService>()
+            //    .AddTransient<OneTimeProductService>()
+            //    .AddTransient<OrderService>()
+            //    .AddTransient<ProductService>()
+            //    .AddTransient<ShopService>()
+            //    .AddTransient<SubscriptionService>()
+            //    .AddTransient<WebhookService>();
+
+            return services;
+        }
+    }
+}
